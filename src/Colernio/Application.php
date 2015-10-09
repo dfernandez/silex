0198879137247dff52a818459f09e282662639ee
David Fernandez <david1983xtc@gmail.com>
2015-10-09 14:56:29 +0200

    Adds Twig

--- src/Colernio/Application.php
+++ src/Colernio/Application.php
@@ -4,17 +4,20 @@ namespace Colernio;
 
 use Colernio\Controllers\IndexController;
 use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Application\TwigTrait;
 
 class Application extends \Silex\Application
 {
+    use TwigTrait;
+
     public function __construct(array $params = [])
     {
         parent::__construct($params);
 
         # Services
-        $this->register(new MonologServiceProvider(), [
-            'monolog.logfile' => 'logs/app.log',
-        ]);
+        $this->register(new MonologServiceProvider(), ['monolog.logfile' => __DIR__.'/../../logs/app.log']);
+        $this->register(new TwigServiceProvider(), ['twig.path' => __DIR__.'/../../views']);
 
         # Controllers
         $this->mountControllers();
--- src/Colernio/Controllers/IndexController.php
+++ src/Colernio/Controllers/IndexController.php
@@ -2,12 +2,16 @@
 
 namespace Colernio\Controllers;
 
+use Colernio\Application;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController
 {
-    public function indexAction()
+    public function indexAction(Application $app)
     {
-        return new Response();
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+
+        return $app->render('indexController/index.twig', [], $response);
     }
 }
